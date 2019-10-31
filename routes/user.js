3c0dfd6209f3cdb7986148962c21a69fb6a7d7f3
hoandreas <andreas_ho@hotmail.com>
2019-11-01 01:25:13 +0700

    google login

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,8 @@
 const router = require('express').Router()
 const UserController = require('../controllers/UserController')
 
-router.post('/', UserController.create)
-router.get('/', UserController.login)
+router.post('/create', UserController.create)
+router.post('/login', UserController.login)
+router.post('/googlelogin', UserController.googlelogin)
 
 module.exports = router
