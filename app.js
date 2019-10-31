cc68bb58fe0530f83dc10103ba3ed3daae59b5fc
hoandreas <andreas_ho@hotmail.com>
2019-10-31 19:42:29 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const routes = require('./routes')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const PORT = process.env.PORT || 3000
+const urlMongoose = 'mongodb://localhost/GroupProjectWeek1'
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+mongoose.connect(urlMongoose, function(err){
+    if(err) console.log('Error connecting to db')
+    else console.log('Success connecting to db')
+})
+
+app.use(cors())
+app.use('/', routes)
+
+app.listen(PORT, () => console.log('Running on port: ' + PORT))
